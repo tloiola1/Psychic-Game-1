9377c42081640c4958d9d3afffb7844911e19e45
Noriah Williams <willnoriah23@gmail.com>
2017-08-22 18:06:49 -0400

    Working on homework

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//Letter choices available
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//Setting all to zero
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+
